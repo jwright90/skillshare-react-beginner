1851f83dd303a876bad3a362bc660084e9374994
Jason <jason.wright90@gmail.com>
2021-07-10 12:42:08 +0100

    fetch jokes from API & handle loading

--- beginner-app/src/App.js
+++ beginner-app/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// A component must only contain a single element
+// We can use a 'fragment' instead of div, i.e. empty <></>
+
+// const Layout = props => (
+//   <>
+//     <header>My Header</header>
+//     <main>{props.children}</main>
+//     <footer>My Footer</footer>
+//   </>
+// );
+
+// When using a class, we have to use render() to output to the page
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+  
+    this.state = {
+      joke: null,
+      isFetchingJoke: false
+    };
+
+    this.onTellJoke = this.onTellJoke.bind(this);
+  }
+
+  fetchJoke() {
+    this.setState({isFetchingJoke : true});
+    fetch('https://icanhazdadjoke.com/', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ 
+          joke: json.joke,
+          isFetchingJoke: false
+         });
+      });
+  };
+
+  componentDidMount() {
+    this.fetchJoke();
+  };
+
+  onTellJoke() {
+    this.fetchJoke();
+  };
+  
+  render() {
+    console.log('--- RENDER ---');
+
+    return (
+      <div>
+        <button onClick={this.onTellJoke} disabled={this.state.isFetchingJoke}>Tell me a joke</button>
+        <p>{this.state.isFetchingJoke ? 'Loading Joke...' : this.state.joke}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
+
